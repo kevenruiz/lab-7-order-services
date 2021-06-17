220eed0bdc368a28afb96b87b52eb5be360a468d
keven-ruiz <kevenruizh@gmail.com>
2021-06-17 16:22:14 -0700

    Initial set up

---
+++ __tests__/app.test.js
@@ -0,0 +1,11 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('testing out the routes for food orders', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('', async)
+});
